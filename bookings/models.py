0777d1778540d77b0d5d5c1b9726fad7fd2c1e10
mehdidov <125817777+mehdidov@users.noreply.github.com>
2025-12-05 16:22:39 +0100

    Creation of bookings/models.py and rooms/model.py

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from rooms.models import Room
 
 # Create your models here.
+Hogwarts_House = [
+    ("Gryffindor","Gryffindor"),
+    ("Slytherin","Slytherin"),
+    ("Ravenclaw,","Hufflepuff"),
+    ("Hufflepuff","Hufflepuff"),
+]
+
+class Bookings(models.Model):
+
+
+    def __str__(self):
+        return 
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-class 
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    size = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
